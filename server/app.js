de10ab691303717565e93d4842eb1f4d05dccb70
mohdafnaan <mohdafnaan833@gmail.com>
2026-02-15 18:33:04 +0530

    chnages

--- server/app.js
+++ server/app.js
@@ -34,17 +34,17 @@ let corsObject = {
 };
 // ================================================================================//
 app.use(cors(corsObject));
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  try {
-    res.status(200).json({ message: "Welcome to the server" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error });
-  }
-});
+// app.get("/", (req, res) => {
+//   try {
+//     res.status(200).json({ message: "Welcome to the server" });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error });
+//   }
+// });
 app.use("/public", userPublicRoutes);
 app.use("/public", adminPublicRoutes);
 app.use("/private", authMiddleware, adminPrivateRoutes);
